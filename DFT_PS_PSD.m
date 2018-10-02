4b86f8520d21c8b874359e8c610d0960dc13c310
Sizhuang Liang <34350306+liangsizhuang@users.noreply.github.com>
2018-10-01 21:37:25 -0400

    Add files via upload

--- DFT_PS_PSD.m
+++ DFT_PS_PSD.m
@@ -1,3 +1,5 @@
+close all;
+
 % Initial Values
 Fs = 100;
 f = 1;
@@ -5,8 +7,16 @@ varphi = 0;
 pause_time = 1;
 pause_time_audio = 0.2;
 
+% Create a figure.
+h_DFT_PS_PSD = figure('name', 'PS_Window');
+
 % Effect of Changing N
 for N = 20:20:320
+    if(isgraphics(h_DFT_PS_PSD))
+       set(0, 'CurrentFigure', h_DFT_PS_PSD)
+    else
+       break;
+    end
     [x,~] = GetSine(f, varphi, Fs, N);
     myPlotDFT_PS_PSD(x, Fs);
     pause(pause_time);
@@ -14,6 +24,11 @@ end
 
 % Effect of Changing varphi
 for varphi = 0:pi/4:2*pi
+    if(isgraphics(h_DFT_PS_PSD))
+       set(0, 'CurrentFigure', h_DFT_PS_PSD)
+    else
+       break;
+    end
     [x,~] = GetSine(f, varphi, Fs, N);
     myPlotDFT_PS_PSD(x, Fs);
     pause(pause_time);
@@ -24,6 +39,11 @@ end
 N_1 = 182-1;
 
 for i = 1:length(x)-N_1
+    if(isgraphics(h_DFT_PS_PSD))
+       set(0, 'CurrentFigure', h_DFT_PS_PSD)
+    else
+       break;
+    end
     myPlotDFT_PS_PSD(x(i:i+N_1,2),Fs);
     pause(pause_time_audio);
 end
--- myPlotDFT_PS_PSD.m
+++ myPlotDFT_PS_PSD.m
@@ -10,8 +10,8 @@ T = N/Fs;
 [px, fpx] = periodogram(x, [], N, Fs, 'centered', 'power');
 % Periodogram ENPSD
 [psdx, fpsdx] = periodogram(x, [], N, Fs, 'centered', 'psd');
+delta_fpsdx = mean(diff(fpsdx));
 
-figure(1);
 subplot(2,2,1);plot(t,x,'.-');xlabel('Time (s)');
 title(['N = ' num2str(N) '    Fs = ' num2str(Fs) ' Hz']);
 text(0.98, 0.95, ['T = ' num2str(T) ' s'], 'Units', 'normalized', 'HorizontalAlignment', 'right');
@@ -27,10 +27,9 @@ title('Power Spectrum');
 text(0.98, 0.95, ['Max Strength = ' num2str(max(px))], 'Units', 'normalized', 'HorizontalAlignment', 'right');
 text(0.98, 0.875, ['Sum = ' num2str(sum(px))], 'Units', 'normalized', 'HorizontalAlignment', 'right');
 
-subplot(2,2,4);stairs(fpsdx-0.5*(1/T), psdx);xlabel('Frequency (Hz)');
+subplot(2,2,4);stairs(fpsdx-0.5*delta_fpsdx, psdx);xlabel('Frequency (Hz)');
 title('ENPSD');
 text(0.98, 0.95, ['Max Value = ' num2str(max(psdx))], 'Units', 'normalized', 'HorizontalAlignment', 'right');
-text(0.98, 0.875, ['Max Value * \Delta f = ' num2str(max(psdx)/T)], 'Units', 'normalized', 'HorizontalAlignment', 'right');
-text(0.98, 0.80, ['\Delta f = 1 / T = ' num2str(1/T)], 'Units', 'normalized', 'HorizontalAlignment', 'right');
+text(0.98, 0.875, ['Integral = ' num2str(sum(psdx)*delta_fpsdx)], 'Units', 'normalized', 'HorizontalAlignment', 'right');
 
 end
